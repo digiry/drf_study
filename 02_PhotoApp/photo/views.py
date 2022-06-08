789828017ffe8ea6fe43cee8852dc7cf08ff9206
Kyujin Kang <digiry@gmail.com>
2022-06-09 01:45:50 +0900

    feat(PhotoApp): add photo_detail

--- 02_PhotoApp/photo/views.py
+++ 02_PhotoApp/photo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from photo.models import Photo
 
@@ -6,3 +6,8 @@ from photo.models import Photo
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, "photo/photo_list.html", {"photos": photos})
+
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, "photo/photo_detail.html", {"photo": photo})
