d5afe72318ce86c6ef370638fd2bbba7780db776
Kyujin Kang <digiry@gmail.com>
2022-06-09 01:38:36 +0900

    feat(PhotoApp): learn jinja2 template 01

--- 02_PhotoApp/photo/views.py
+++ 02_PhotoApp/photo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from photo.models import Photo
+
 
 def photo_list(request):
-    return render(request, "photo/photo_list.html", {})
+    photos = Photo.objects.all()
+    return render(request, "photo/photo_list.html", {"photos": photos})
