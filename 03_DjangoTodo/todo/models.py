42421549baf0a5c98ef8cc7f69dfbe6aee212fac
Kyujin Kang <digiry@gmail.com>
2022-06-16 01:35:24 +0900

    feat(DjangoTodo): add DjangoTodo Web

---
+++ 03_DjangoTodo/todo/models.py
@@ -0,0 +1,13 @@
+from typing import Any
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+
+    def __str__(self) -> Any:
+        return self.title
---
+++ 03_DjangoTodo/todo/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import redirect, render
+
+from todo.forms import TodoForm
+from todo.models import Todo
+
+
+def todo_list(request):
+    todos = Todo.objects.filter(complete=False)
+    return render(request, 'todo/todo_list.html', {'todos': todos})
+
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
+
+
+def todo_post(request):
+    form = TodoForm()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+
+    return render(request, 'todo/todo_post.html', {"form": form})
+
+
+def todo_edit(request, pk):
+    todo = Todo.objects.get(id=pk)
+    form = TodoForm(instance=todo)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+
+    return render(request, 'todo/todo_post.html', {"form": form})
+
+
+def done_list(request):
+    dones = Todo.objects.filter(complete=True)
+    return render(request, 'todo/done_list.html', {"dones": dones})
+
+
+def todo_done(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.complete = True
+    todo.save()
+    return redirect('todo_list')
