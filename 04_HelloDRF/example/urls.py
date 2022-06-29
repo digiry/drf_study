4876b753defa5260d72a72451987668c8a045256
Kyujin Kang <digiry@gmail.com>
2022-06-30 01:26:08 +0900

    feat(HelloDRF): add DRF mixins

--- 04_HelloDRF/example/urls.py
+++ 04_HelloDRF/example/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from example.views import helloAPI, booksAPI, bookAPI, BookAPI, BooksAPI
+from example.views import (
+    helloAPI,
+    booksAPI,
+    bookAPI,
+    BookAPI,
+    BooksAPI,
+    BooksAPIMixins,
+    BookAPIMixins,
+)
 
 urlpatterns = [
     path("hello/", helloAPI),
@@ -7,4 +15,6 @@ urlpatterns = [
     path("fbv/book/<int:bid>/", bookAPI),
     path("cbv/books/", BooksAPI.as_view()),
     path("cbv/book/<int:bid>/", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
 ]
