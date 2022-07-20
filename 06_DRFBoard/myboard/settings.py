a69acd6d8c4fd497b96d09c47284c090956ad21a
Kyujin Kang <digiry@gmail.com>
2022-07-21 00:42:08 +0900

    feat(06_DRFBoard): add Posts and Comments

--- 06_DRFBoard/myboard/settings.py
+++ 06_DRFBoard/myboard/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "users",
     "corsheaders",
+    "posts",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -113,6 +115,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
 }
 
 # Internationalization
--- 06_DRFBoard/users/urls.py
+++ 06_DRFBoard/users/urls.py
@@ -5,5 +5,5 @@ from users.views import RegisterView, LoginView, ProfileView
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),
-    path("profile/<int:pk>/", ProfileView.as_view()),
+    path("profile/", ProfileView.as_view()),
 ]
